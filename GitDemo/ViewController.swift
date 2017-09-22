bf0c54d294a42ca2cc70328f037eab6f74a007a1
KamaljeetPunia <lotoe14@gmail.com>
2017-09-22 11:07:02 +0530

    This is my first commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Apple on 9/22/17.
+//  Copyright © 2017 xpWallet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
