f036e7409f17376bc8a8a012d9714f5514f7d89b
KamaljeetPunia <lotoe14@gmail.com>
2017-09-22 11:47:34 +0530

    Map feature addes

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("This is my first git project")
+        let mapView = MapViewController()
+        print(mapView)
     }
 
     override func didReceiveMemoryWarning() {
